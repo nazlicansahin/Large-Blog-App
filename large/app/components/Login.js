0520a831065539d1724109b4fae98626fc8a1f10
Nazlıcan Şahin <76625343+nazlicansahin@users.noreply.github.com>
2024-01-14 01:17:37 +0300

    Delete unused components

--- large/app/components/Login.js
+++
@@ -1,8 +0,0 @@
-import Image from "next/image";
-import Link from "next/link";
-import { Formik, Form, Field, ErrorMessage } from 'formik';
-import * as Yup from 'yup';
-const Login = () => {
-
-}
-export default Login
